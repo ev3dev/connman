f44425a5997c9dabfea72802c5b0bfe4623e0554
Daniel Wagner <daniel.wagner@bmw-carit.de>
2016-11-10 19:35:05 +0100

    wispr: Merge two DBGs into one
    There is little point in having two DBGs for this. Let's merge them.

--- src/wispr.c
+++ src/wispr.c
@@ -832,8 +832,8 @@ static int wispr_portal_detect(struct connman_wispr_portal_context *wp_context)
 	int err = 0;
 	int i;
 
-	DBG("wispr/portal context %p", wp_context);
-	DBG("service %p", wp_context->service);
+	DBG("wispr/portal context %p service %p", wp_context,
+		wp_context->service);
 
 	service_type = connman_service_get_type(wp_context->service);
 
