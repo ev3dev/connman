e1afb7c6a1a4b1c8b92bd2f85456c86986be3aaf
Daniel Wagner <daniel.wagner@bmw-carit.de>
2016-11-10 19:35:42 +0100

    rtnl: Remove useless DBG
    No real value in this DBG. Let's remove it.

--- src/rtnl.c
+++ src/rtnl.c
@@ -1374,8 +1374,6 @@ static int process_response(guint32 seq)
 
 static void rtnl_message(void *buf, size_t len)
 {
-	DBG("buf %p len %zd", buf, len);
-
 	while (len > 0) {
 		struct nlmsghdr *hdr = buf;
 		struct nlmsgerr *err;
